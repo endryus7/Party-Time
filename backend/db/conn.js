fa1ab37e140bba3d6b4129c5cb30e5fe5c615933
Endryus-Montezano <endryusmontezano@live.com>
2026-02-18 15:49:31 -0300

    Criando o backend
    Conecta ao banco, cria os models e cria estruturas de serviço

---
+++ backend/db/conn.js
@@ -0,0 +1,16 @@
+const mongoose = requer("mongoose")
+
+async function main() {
+    
+    try {
+        mongoose.set("strictQuery", true)
+
+        await mongoose.connect("mongodb+srv://endryusmontezano_db_user:<caveira1>@party-time.92qdabl.mongodb.net/?appName=Party-Time")
+
+        console.log("Conectado ao banco!")
+    } catch (error) {
+        console.log(`Erro: ${error}`)
+    }
+}
+
+module.exports = main
