57398d0a8bf3a5760681eeb6122a261b63a00eff
Endryus-Montezano <endryusmontezano@live.com>
2026-02-19 19:05:22 -0300

    Configura o Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 
+import { Outlet } from 'react-router-dom'
+
 function App() {
   
 
   return (
     <div className="App">
       <h1>Party Time</h1>
+      <Outlet />
     </div>
   )
 }
