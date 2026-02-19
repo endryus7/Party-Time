25d1e25c5a182bb311b612fe39284b53aec9d751
Endryus-Montezano <endryusmontezano@live.com>
2026-02-19 16:19:49 -0300

    Resgata todas as festas

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -34,6 +34,17 @@ const partyController = {
         } catch (error) {
             console.log(error)
         }
+    },
+
+    getAll: async(req, res) => {
+        try {
+            const parties = await PartyModel.find()
+
+            res.json(parties)
+            
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 
