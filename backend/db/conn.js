864234fd6c68bf9eb4ff14fdcbd71db7feaa0f73
Endryus-Montezano <endryusmontezano@live.com>
2026-02-19 00:23:07 -0300

    Adiciona dados no banco

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,11 +1,11 @@
-const mongoose = requer("mongoose")
+const mongoose = require("mongoose")
 
 async function main() {
     
     try {
         mongoose.set("strictQuery", true)
 
-        await mongoose.connect("mongodb+srv://endryusmontezano_db_user:<caveira1>@party-time.92qdabl.mongodb.net/?appName=Party-Time")
+        await mongoose.connect("mongodb+srv://endryusmontezano_db_user:caveira1@party-time.92qdabl.mongodb.net/?appName=Party-Time")
 
         console.log("Conectado ao banco!")
     } catch (error) {
--- backend/routes/services.js
+++ backend/routes/services.js
@@ -3,5 +3,6 @@ const router = require("express").Router()
 const serviceController = require("../controllers/serviceController")
 
 // Funções
+router.route("/services").post((req, res) => serviceController.create(req, res))
 
 module.exports = router
