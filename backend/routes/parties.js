943d668af3f0af5076785d421f4b21dd40cc8b35
Endryus-Montezano <endryusmontezano@live.com>
2026-02-19 16:35:13 -0300

    Resgata festa individual

--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -10,4 +10,8 @@ router
     .route("/parties")
     .get((req, res) => partyController.getAll(req, res))
 
+router
+    .route("/parties/:id")
+    .get((req, res) => partyController.get(req, res))
+
 module.exports = router
