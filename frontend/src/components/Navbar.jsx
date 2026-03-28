627f282ecfd41d447765af61bfaa91ea1480599a
Endryus-Montezano <endryusmontezano@live.com>
2026-03-28 15:55:06 -0300

    Modifica o Navbar.jsx e Navbar.css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,25 @@
 import { NavLink } from "react-router-dom";
-
-import "./Navbar.css"
+import "./Navbar.css";
 
 const Navbar = () => {
-    return(
-        <nav id="navbar">
-            <h2>Party Time!</h2>
-            <ul>
-                <li>
-                    <NavLink to="/">Minhas Festas</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/party/new" className="btn">Criar Festa</NavLink>
-                </li>
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav id="navbar">
+      <NavLink to="/" className="logo">
+        <span>🎉</span>
+        <h2>Party Time!</h2>
+      </NavLink>
+      <ul>
+        <li>
+          <NavLink to="/">Minhas Festas</NavLink>
+        </li>
+        <li>
+          <NavLink to="/party/new" className="btn">
+            Criar Festa
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
